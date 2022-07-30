c4b1d7b62a2583f0f6304b4d9a673d235256187c
izzumani <isaac.njiru@gmail.com#>
2022-07-30 04:09:39 +0300

    Update Library

--- EFCore_DBLibrary/InventoryDbContext.cs
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.IO;
+using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCore_DBLibrary
 {
     public class InventoryDbContext: DbContext
     {
+        private static IConfigurationRoot? _configuration;
+
+        public DbSet<Item> Items { get; set; }
+
         // Add a default constructor if scaffolding is needed
         public InventoryDbContext() { }
         //Add the complex constructor for allowing Dependency Injection
@@ -11,7 +19,24 @@ namespace EFCore_DBLibrary
         {
             
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog= InventoryManagerDb;User ID=sa;Password=friend");
+                var builder = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json", optional: true,reloadOnChange: true);
+                 _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
 
+            }
+        }
+
+        
+
+        
 
     }
 }
