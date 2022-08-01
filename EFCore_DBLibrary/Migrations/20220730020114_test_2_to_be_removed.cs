f0e759085e9f077e2f486e684d15419cf7675536
izzumani <isaac.njiru@gmail.com#>
2022-08-01 04:25:18 +0300

    include migration files

---
+++ EFCore_DBLibrary/Migrations/20220730020114_test_2_to_be_removed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DBLibrary.Migrations
+{
+    public partial class test_2_to_be_removed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ToBeRemoved",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ToBeRemoved",
+                table: "Items");
+        }
+    }
+}
---
+++ EFCore_DBLibrary/Migrations/20220730204517_updateItem_enforceKeyAndDefaultValue1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DBLibrary.Migrations
+{
+    public partial class updateItem_enforceKeyAndDefaultValue1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Items");
+        }
+    }
+}
